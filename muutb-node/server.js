d422e1e77e9bde30a4ccbadc6581fd7e56c23e67
DATIUS DANIEL DAMIAN <datiuslink@gmail.com>
2025-08-29 07:10:57 +0300

    Initial commit: setup frontend and backend for muutb

---
+++ muutb-node/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3999;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
